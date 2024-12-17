f870917a500dea8d0cf0a40ce7771dcdea94c0dd
Moritz Dahm <94791958+DerMoehre@users.noreply.github.com>
2024-12-17 13:20:50 +0100

    initial commit

---
+++ Home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Forecast",
+    page_icon="📈",
+)
+
+st.title("Welcome to TimeSeries Forecasting")
+
+st.markdown(
+    """
+    Streamlit is an open-source app framework built specifically for
+    Machine Learning and Data Science projects.
+    **👈 Select a demo from the sidebar** to see some examples
+    of what Streamlit can do!
+"""
+)
---
+++ pages/1Upload_your_Data.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+import csv
+
+
+def detect_delimiter(data_string):
+    """Detect the delimiter of a CSV file"""
+    try:
+        sniffer = csv.Sniffer()
+        delimiter = sniffer.sniff(data_string).delimiter
+        return delimiter
+    except Exception as e:
+        return ","
+
+
+def load_file(file):
+    try:
+        data_string = file.getvalue().decode("utf-8")
+        delimiter = detect_delimiter(data_string)
+        df = pd.read_csv(uploaded_file, delimiter=delimiter)
+        return df
+    except Exception as e:
+        return e
+
+
+st.set_page_config(page_title="Upload Data", page_icon="⬆️")
+
+st.title("Upload your data")
+st.sidebar.header("Upload your data")
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+    dataframe = load_file(uploaded_file)
+    st.write(dataframe)
