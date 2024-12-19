209d110cb99e9d97762ed8b20adc644caf9906a2
Moritz Dahm <94791958+DerMoehre@users.noreply.github.com>
2024-12-19 13:29:47 +0100

    added plot function to fitting

--- pages/2Model_Fitting.py
+++ pages/2Model_Fitting.py
@@ -1,14 +1,17 @@
 import streamlit as st
 import plotly.express as px
 
-from shared.utils_fitting import load_data_from_session, train_test_split
+from shared.utils_fitting import (
+    load_data_from_session,
+    train_test_split,
+    return_imported_models,
+    test_train_plot,
+)
+from shared.utils_upload import is_data_in_session
 
 st.set_page_config(page_title="Model Fitting", page_icon="🦾", layout="wide")
 
-
 st.title("Model Fitting 🦾")
-
-x_axis, y_axis, data = load_data_from_session()
 
 # Sidebar
 st.sidebar.header("Train-Test Split")
@@ -16,30 +19,15 @@ split_ratio = st.sidebar.slider(
     "Select Train-Test Split Ratio (%)", min_value=50, max_value=90, value=70, step=1
 )
 
-train_data, test_data = train_test_split(data, split_ratio)
-
-# Plot
-fig = px.line(data, x="ds", y="y", title=f"{x_axis} vs {y_axis}")
-fig.update_layout(xaxis_title=x_axis, yaxis_title=y_axis)
-fig.update_traces(opacity=0.1)
-
-fig.add_scatter(
-    x=train_data["ds"],
-    y=train_data["y"],
-    mode="lines",
-    name="Train Data",
-    line=dict(color="blue", width=4),
+model_list = return_imported_models()
+model = st.sidebar.selectbox(
+    "Select Model", [model_list[model] for model in model_list]
 )
-fig.add_scatter(
-    x=test_data["ds"],
-    y=test_data["y"],
-    mode="lines",
-    name="Test Data",
-    line=dict(color="red", width=4),
-)
-fig.update_layout(xaxis_title="Date", yaxis_title="Values", legend_title="Data Split")
 
-st.write(
-    f"### Train-Test Split Visualization ({split_ratio}% Train, {100 - split_ratio}% Test)"
-)
-st.plotly_chart(fig, use_container_width=True)
+if is_data_in_session():
+    x_axis, y_axis, data = load_data_from_session()
+    train_data, test_data = train_test_split(data, split_ratio)
+    test_train_plot(data, train_data, test_data, x_axis, y_axis, split_ratio)
+
+else:
+    st.error("You have to first update Data on the upload page.")
--- shared/utils_fitting.py
+++ shared/utils_fitting.py
@@ -3,6 +3,15 @@ import csv
 import plotly.express as px
 import pandas as pd
 
+from statsforecast import StatsForecast
+from statsforecast.models import (
+    AutoARIMA,
+    SeasonalNaive,
+    HoltWinters,
+    HistoricAverage,
+    MSTL,
+)
+
 ####### PAGE 2: Model Fitting #######
 
 
@@ -23,3 +32,44 @@ def train_test_split(data, split_ratio):
     test_data = data.iloc[train_size:]
 
     return train_data, test_data
+
+
+def return_imported_models():
+    return {
+        "AutoARIMA": AutoARIMA,
+        "SeasonalNaive": SeasonalNaive,
+        "HoltWinters": HoltWinters,
+        "HistoricAverage": HistoricAverage,
+        "MSTL": MSTL,
+    }
+
+
+def test_train_plot(data, train_data, test_data, x_axis, y_axis, split_ratio):
+    fig = px.line(data, x="ds", y="y", title=f"{x_axis} vs {y_axis}")
+    fig.update_layout(xaxis_title=x_axis, yaxis_title=y_axis)
+    fig.update_traces(opacity=0.1)
+
+    fig.add_scatter(
+        x=train_data["ds"],
+        y=train_data["y"],
+        mode="lines",
+        name="Train Data",
+        line=dict(color="blue", width=4),
+    )
+    fig.add_scatter(
+        x=test_data["ds"],
+        y=test_data["y"],
+        mode="lines",
+        name="Test Data",
+        line=dict(color="red", width=4),
+    )
+    fig.update_layout(
+        xaxis_title="Date", yaxis_title="Values", legend_title="Data Split"
+    )
+
+    st.write(
+        f"### Train-Test Split Visualization ({split_ratio}% Train, {100 - split_ratio}% Test)"
+    )
+    st.plotly_chart(fig, use_container_width=True)
+
+    # return
